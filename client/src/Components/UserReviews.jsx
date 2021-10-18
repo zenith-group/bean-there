e385c19295fc6a1be9cbfaa932e050a380f76ed1
Ye Feng Chen <yefengchen96@gmail.com>
2021-10-18 11:18:38 -0400

    User reivews page set up

---
+++ client/src/Components/UserReviews.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const UserReviews = ({ reviews }) => (
+  <div id='user-review-container'>
+    <h2>Account Activities</h2>
+    {reviews.map((review, index) => {
+      <div id='review-container' key={`reviews_${}`}>
+        <h3 className='review.business_id'></h3>
+
+      </div>
+    })}
+  </div>
+)
+
+export default UserReviews;
