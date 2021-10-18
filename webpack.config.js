8850d9ef03867921209eb51f5dd12e2d0c712294
Chris Chung <chris.ch.987@gmail.com>
2021-10-18 14:22:37 -0400

    Revert webpack changes and removed async await, refactored code to make it unnecessary

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const { SourceMapDevToolPlugin } = require('webpack');
 //simport css from 'file.css';
 
 module.exports = {
-  entry: ['@babel/polyfill', path.join(__dirname, 'client/src/index.jsx')],
+  entry: path.join(__dirname, 'client/src/index.jsx'),
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'client/public'),
@@ -16,7 +16,6 @@ module.exports = {
         loader: 'babel-loader',
         options: {
           presets: ['@babel/preset-env', '@babel/preset-react'],
-          plugins: ['@babel/transform-runtime'],
         },
       },
       {
