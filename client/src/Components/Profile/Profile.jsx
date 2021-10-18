4a5f476946a407271eb1934d8a35b33eb3bf52a1
Ye Feng Chen <yefengchen96@gmail.com>
2021-10-18 14:21:22 -0400

    User profile complete, need firebase to finish

---
+++ client/src/Components/Profile/Profile.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import UserReviews from './UserReviews.jsx';
+
+const Profile = () => (
+  <div id='profile'>
+    <UserReviews />
+  </div>
+)
+
+export default Profile;
--- client/src/Components/Profile/UserReviews.jsx
+++ client/src/Components/Profile/UserReviews.jsx
@@ -4,9 +4,12 @@ const UserReviews = ({ reviews }) => (
   <div id='user-review-container'>
     <h2>Account Activities</h2>
     {reviews.map((review, index) => {
-      <div id='review-container' key={`reviews_${}`}>
+      <div id='review-container' key={`reviews_${index}`}>
         <h3 className='store_id'>{review.store_id}</h3>
-
+        <h4 className='coffee_type'>{review.coffee_type}</h4>
+        <h4 className='rating'>{review.rating}</h4>
+        <h5 className='review_body'>{review.body}</h5>
+        <h4 className='date'>{review.date}</h4>
       </div>
     })}
   </div>
