74833278363adcb997608954319b02962a7c089b
Chris Chung <chris.ch.987@gmail.com>
2021-10-18 11:00:28 -0400

    Complete basic search bar functionality with a few default coffee values

--- client/src/Components/SearchBarComponents/Categories.jsx
+++ client/src/Components/SearchBarComponents/Categories.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Select from 'react-select';
 
-const Categories = () => {
-  return <div>Categories</div>;
+const options = [
+  { value: 'espresso', label: 'Espresso' },
+  { value: 'latte', label: 'Latte' },
+  { value: 'cappuccino', label: 'Cappuccino' },
+];
+
+const Categories = ({ categories, handleChange }) => {
+  return <Select isMulti onChange={handleChange} options={options} />;
 };
 
 export default Categories;
--- client/src/Components/SearchBarComponents/Input.jsx
+++ client/src/Components/SearchBarComponents/Input.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Input = ({ input, handleChange }) => {
-  return <input type='text' value={input} onChange={handleChange} />;
+  return (
+    <input
+      name='input'
+      type='text'
+      value={input}
+      onChange={handleChange}
+      placeholder='Search for store'
+    />
+  );
 };
 
 export default Input;
