7175795b91fc4de087172853d6ee44344daf8c05
Ye Feng Chen <yefengchen96@gmail.com>
2021-10-19 11:27:55 -0400

    Removed Autocomplete

--- client/src/Components/SearchBarComponents/Location.jsx
+++ client/src/Components/SearchBarComponents/Location.jsx
@@ -1,27 +1,14 @@
-import React,{useState} from 'react';
-import { Autocomplete } from '@react-google-maps/api';
+import React from 'react';
 
 const Location = ({ location, handleChange }) => {
-
-  const [autocomplete, setAutoComplete] = useState(null)
-
-  const onLoad = (autoC) => setAutoComplete(autoC)
-  const onPlaceChange =  () => {
-    const lat = autocomplete.getPlace().geometry.location.lat()
-    const lng = autocomplete.getPlace().geometry.location.lng()
-    console.log(lat, lng)
-  }
-
   return (
-    <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChange}>
-      <input
-        name='location'
-        type='text'
-        value={location}
-        onChange={handleChange}
-        placeholder='Location'
-      />
-    </Autocomplete>
+    <input
+      name='location'
+      type='text'
+      value={location}
+      onChange={handleChange}
+      placeholder='Location'
+    />
   );
 };
 
