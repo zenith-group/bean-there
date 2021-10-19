ff0279ad58f9e42d5d69b07698ecb9d505d58dc4
Ye Feng Chen <yefengchen96@gmail.com>
2021-10-19 11:35:19 -0400

    Logo click now leads back to home page

--- client/src/Components/Headers/HomeHeader.jsx
+++ client/src/Components/Headers/HomeHeader.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const HomeHeader = ({ loggedin, user, onClick }) => (
   <div id='home-header' className='header'>
-    <img className='logo' src={'./img/Bean_There.png'} alt='logo' />
+    <Link to='/'><img className='logo' src={'./img/Bean_There.png'} alt='logo' /></Link>
     <div className='nav-bar'>
       {loggedin ? <button><Link to='/profile'>Accounts</Link></button> : <button><Link to='login'>Login</Link></button>}
       {loggedin ? <button onClick={() => {onClick()}}><Link to='/'>Sign Out</Link></button> : <button><Link to='signup'>Sign Up</Link></button>}
