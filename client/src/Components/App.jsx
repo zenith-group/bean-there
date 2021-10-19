06c56683579811ffcee0c70bdba92e7018c19ecc
Chris Chung <chris.ch.987@gmail.com>
2021-10-19 15:14:28 -0400

    Add coffee types list to searchBar and update code consistency across App.jsx

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomeHeader from './Headers/HomeHeader.jsx';
 import Header from './Headers/Header.jsx';
 import SearchBar from './SearchBarComponents/SearchBar.jsx';
 import Map from './Map/Map.jsx';
-import Review from './Review/Review.jsx'
+import Review from './Review/Review.jsx';
 import Profile from './Profile/Profile.jsx';
 
 class App extends React.Component {
@@ -19,8 +20,12 @@ class App extends React.Component {
       currentLocation: {},
       loggedin: true,
       userReviews: [],
-      user: {}
+      user: {},
     };
+    this.getCurrentLocation = this.getCurrentLocation.bind(this);
+    this.updateSearch = this.updateSearch.bind(this);
+    this.onSignoutClick = this.onSignoutClick.bind(this);
+    this.getCoffeeTypes = this.getCoffeeTypes.bind(this);
   }
 
   updateSearch(term, coffeeList, location) {
@@ -31,30 +36,49 @@ class App extends React.Component {
     });
   }
 
-  getCurrentLocation = () => {
-    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
-      this.setState({
-        currentLocation: { lat: latitude, lng: longitude }
-      })
-    })
+  getCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        this.setState({
+          currentLocation: { lat: latitude, lng: longitude },
+        });
+      }
+    );
   }
 
-  fetchUserReviews = (user_id)  => {
-    axios.get(`/reviews/users/?user_id=${user_id}`)
-      .then(result => {
+  fetchUserReviews(user_id) {
+    axios
+      .get(`/reviews/users/?user_id=${user_id}`)
+      .then((result) => {
         this.setState({
-          userReviews: result.data
+          userReviews: result.data,
         });
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
-      })
-  };
+      });
+  }
 
   onSignoutClick() {
     this.setState({
-      loggedin: false
+      loggedin: false,
     });
+  }
+
+  getCoffeeTypes() {
+    axios
+      .get('/types')
+      .then((res) => {
+        let coffees = res.data.map((coffeeType) => coffeeType.name);
+        this.setState({ searchCoffeeList: coffees });
+      })
+      .catch((err) => {
+        console.err(err);
+      });
+  }
+
+  componentDidMount() {
+    this.getCoffeeTypes();
   }
 
   render() {
@@ -63,29 +87,36 @@ class App extends React.Component {
         <div className='app'>
           <Switch>
             <Route exact path='/'>
-              <HomeHeader loggedin={this.state.loggedin} user={this.state.user} onClick={this.onSignoutClick.bind(this)}/>
+              <HomeHeader
+                loggedin={this.state.loggedin}
+                user={this.state.user}
+                onClick={this.onSignoutClick}
+              />
               <SearchBar
-                updateSearch={this.updateSearch.bind(this)}
-                updateLocation={this.getCurrentLocation.bind(this)}
+                updateSearch={this.updateSearch}
+                updateLocation={this.getCurrentLocation}
+                coffeeList={this.state.searchCoffeeList}
               />
             </Route>
             <Route path='/search'>
               <Header
                 loggedin={this.state.loggedin}
                 user={this.state.user}
-                onClick={this.onSignoutClick.bind(this)}
-                updateSearch={this.updateSearch.bind(this)}
-                updateLocation={this.getCurrentLocation.bind(this)}/>
-              <Map currentLocation={this.state.currentLocation}/>
+                onClick={this.onSignoutClick}
+                updateSearch={this.updateSearch}
+                updateLocation={this.getCurrentLocation}
+              />
+              <Map currentLocation={this.state.currentLocation} />
             </Route>
             <Route path='/profile'>
               <Header
                 loggedin={this.state.loggedin}
                 user={this.state.user}
-                onClick={this.onSignoutClick.bind(this)}
-                updateSearch={this.updateSearch.bind(this)}
-                updateLocation={this.getCurrentLocation.bind(this)}/>
-              <Profile reviews={this.state.userReviews}/>
+                onClick={this.onSignoutClick}
+                updateSearch={this.updateSearch}
+                updateLocation={this.getCurrentLocation}
+              />
+              <Profile reviews={this.state.userReviews} />
             </Route>
           </Switch>
         </div>
