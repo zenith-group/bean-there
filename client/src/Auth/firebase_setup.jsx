c1d00f815f48e40f467a2422c9b116245973190e
Charles Ewing <charlesewing3@gmail.com>
2021-10-19 13:57:21 -0400

    Move firebaseConfig object to ignored config.js file

--- client/src/Auth/firebase_setup.jsx
+++ client/src/Auth/firebase_setup.jsx
@@ -1,17 +1,8 @@
 // Initial set-up (https://firebase.google.com/docs/web/setup)
 
 import { initializeApp } from 'firebase/app';
+import KEYS from '../../../config.js';
 
-// team gmail config:
-const firebaseConfig = {
-    apiKey: "AIzaSyCR8QIwas5kmDeFld0kau5MoE7aDB9jBJk",
-    authDomain: "bean-there-1a5aa.firebaseapp.com",
-    projectId: "bean-there-1a5aa",
-    storageBucket: "bean-there-1a5aa.appspot.com",
-    messagingSenderId: "496416558832",
-    appId: "1:496416558832:web:62da1faa6c8c6dd869fc35"
-  };
-
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(KEYS.firebaseConfig);
 
 export default app;
