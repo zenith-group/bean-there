0e6a64a41aae335da23a4a1164aa9a847acd4342
Shavkat Shavkiev <shavkatshavkiev2002@gmail.com>
2021-10-20 16:06:07 -0400

    Added space after getYelp in HomePage.jsx

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import SearchBar from '../SearchBarComponents/SearchBar.jsx';
+import React from "react";
+import SearchBar from "../SearchBarComponents/SearchBar.jsx";
 
-const HomePage = ({ getYelp,updateSearch, updateLocation, coffeeList }) => (
-  <div id='home-page'>
-    <SearchBar updateSearch={updateSearch} updateLocation={updateLocation} coffeeList={coffeeList} getYelp={getYelp}/>
+const HomePage = ({ getYelp, updateSearch, updateLocation, coffeeList }) => (
+  <div id="home-page">
+    <SearchBar
+      updateSearch={updateSearch}
+      updateLocation={updateLocation}
+      coffeeList={coffeeList}
+      getYelp={getYelp}
+    />
   </div>
-)
+);
 
 export default HomePage;
