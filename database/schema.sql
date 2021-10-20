624145e5cbd95d0a06af728786fc22825f374178
Shavkat Shavkiev <shavkatshavkiev2002@gmail.com>
2021-10-20 17:18:24 -0400

    Installed postgres

--- database/schema.sql
+++ database/schema.sql
@@ -1,19 +1,16 @@
 -- INSTRUCTIONS TO RUN THIS FILE:
-    -- npm install (so postgres will install)
-    -- cd into "database" directory
-    -- run command: psql postgres
-    -- run command: \i schema.sql
-    -- if it works, your two empty tables will print in the console
-    -- now follow the instructions in the "data.sql" file
-    -- NOTE: we're using booleans for hot/cold in the coffee_type table. Hot = TRUE, Cold = FALSE
-
+-- npm install (so postgres will install)
+-- cd into "database" directory
+-- run command: psql postgres
+-- run command: \i schema.sql
+-- if it works, your two empty tables will print in the console
+-- now follow the instructions in the "data.sql" file
+-- NOTE: we're using booleans for hot/cold in the coffee_type table. Hot = TRUE, Cold = FALSE
 -- Create Database
 DROP DATABASE IF EXISTS coffee;
 CREATE DATABASE coffee;
-
 -- Connect to Database
-\c coffee;
-
+\ c coffee;
 -- Create Table 'reviews'
 DROP TABLE IF EXISTS reviews;
 CREATE TABLE reviews (
@@ -30,31 +27,29 @@ CREATE TABLE reviews (
   report BOOLEAN DEFAULT FALSE,
   PRIMARY KEY (review_id)
 );
-
 -- Create Table 'coffee_type'
 DROP TABLE IF EXISTS coffee_type;
 CREATE TABLE coffee_type (
   coffee_id SERIAL,
   name VARCHAR NULL DEFAULT NULL,
-  temperature BOOLEAN DEFAULT TRUE, -- HOT = TRUE, COLD = FALSE
+  temperature BOOLEAN DEFAULT TRUE,
+  -- HOT = TRUE, COLD = FALSE
   PRIMARY KEY (coffee_id)
 );
-
 -- Foreign Key
-ALTER TABLE reviews ADD FOREIGN KEY (coffee_type) REFERENCES coffee_type (coffee_id);
-
+ALTER TABLE reviews
+ADD FOREIGN KEY (coffee_type) REFERENCES coffee_type (coffee_id);
 -- -- Insert Test Data
 -- INSERT INTO coffee_type (name) VALUES ('mocha');
 -- INSERT INTO reviews (user_id,coffee_type,rating,review_body,date,store_id,helpful,report) VALUES ('testuserID',1,'3','great coffee',current_timestamp,'123456','false','false');
-
 -- Log the newly created tables
-SELECT * FROM coffee_type;
-SELECT * FROM reviews;
-
+SELECT *
+FROM coffee_type;
+SELECT *
+FROM reviews;
 -- -- Delete Test Data
 -- DELETE FROM reviews WHERE review_id = 1;
 -- DELETE FROM coffee_type WHERE coffee_id = 1;
-
 -- -- Log the empty tables
 -- SELECT * FROM coffee_type;
 -- SELECT * FROM reviews;
