7e0031d958cfb84e0d5e635bbda971cb8c226203
Charles Ewing <charlesewing3@gmail.com>
2021-10-22 12:00:11 -0400

    Possible Bean There descrip in footer

--- client/src/Components/Footers/Footer.jsx
+++ client/src/Components/Footers/Footer.jsx
@@ -32,28 +32,27 @@ class Footer extends React.Component {
             <div>
               <a href='https://www.google.com/maps/place/Squid+St,+Portsmouth,+NH+03801/@43.021721,-70.79349,17z/data=!3m1!4b1!4m5!3m4!1s0x89e2c1cae2b65501:0xee600e9cbfdee808!8m2!3d43.021721!4d-70.791296' target="_blank"><i className="fa fa-map-marker"></i></a>
               <p>
-                <span>456 squid St</span> Portsmouth, NH
+                <span>456 Squid St</span> Portsmouth, NH
               </p>
             </div>
 
             <div>
               <i className="fa fa-phone"></i>
-              <p>+1.555.555.5555</p>
+              <p>+1.800.456.BEANS</p>
             </div>
 
             <div>
               <i className="fa fa-envelope"></i>
               <p>
-                <a href="mailto:support@company.com">support@company.com</a>
+                <a href="mailto:support@beans.com">support@beans.com</a>
               </p>
             </div>
           </div>
 
           <div className="footer-right">
             <p className="footer-company-about">
-              <span>About the company</span>
-              Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce
-              euismod convallis velit, eu auctor lacus vehicula sit amet.
+              <span>About Bean There</span>
+              Established in 2021, Bean There is a no-nonsense website helping the modern caf<i style={{margin: "0px"}}>fiend</i> find their favorite blend. Search for shops by location and compare reviews based on coffee-type alone. Backed by an industry-leading no-tea-guarantee.
             </p>
 
             <div className="footer-icons">
